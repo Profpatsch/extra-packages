4284a2663d0804c234fd651be8fdf05ffff02b35
Profpatsch <mail@profpatsch.de>
2016-02-28 02:53:03 +0100

    skip vm
    since it’s only packages, no modules (yet)

--- release.nix
+++ release.nix
@@ -1,10 +1,6 @@
 with import <nixpkgs> {};
 {
-  packages = (import <nixpkgs/nixos> {
-    configuration = {
-      system.extraDependencies = [
-        haskellPackages.reactive-banana-wx
-      ];
-    };
-  }).vm;
+  haskellPackages = {
+    inherit (pkgs.haskellPackages) reactive-banana-wx;
+  };
 }
