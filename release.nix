dba2dfe01a7b3adcaea2885de9a02899481dbd6c
Profpatsch <mail@profpatsch.de>
2016-04-12 15:05:45 +0200

    haskell: some lts packages

--- release.nix
+++ release.nix
@@ -7,12 +7,19 @@ let hp = haskellPackages.override {
         (enableCabalFlag super.reactive-banana-wx "buildexamples")
         [ super.random super.executable-path ];
     };
+    inherit purescript;
   };
+
+   hp_lts = haskell.packages.lts-5_9;
 
 in
 {
   haskellPackages = {
     inherit (hp) reactive-banana-wx pandoc;
+  };
+
+  haskellPackages_lts = {
+    inherit (hp_lts) cassava HDBC HDBC-odbc wx wxcore;
   };
 
   # quasselDaemonStatic = kde5.quasselDaemon.override { static = true; };
