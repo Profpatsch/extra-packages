83575eac05ef689b6ec31102e96b25e92a23ddc9
Profpatsch <mail@profpatsch.de>
2016-07-27 15:43:32 +0200

    add working ghcjs version & reflex

--- release.nix
+++ release.nix
@@ -22,8 +22,17 @@ in
     in selection // { hooglePkgs = (hp_lts.ghcWithHoogle (_: lib.mapAttrsToList (_: v: v) selection)); };
 
 
-  haskellPackages_ghcjs = {
+  haskellPackages_ghcjs =
+    let oldpkgs = import (nixpkgs.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          # last change of ghcjs
+          rev = "d3754d34824810d80c8e05767f05c85855883a00";
+          sha256 = "1ncqfsb66jw8f8gxahrx3ia4hh9vlhfswzcp2id8sm2jkm5dmyid";
+        }) {};
+   in {
     haskell.compiler.ghcjs = haskell.compiler.ghcjs;
+    oldghcjs = { inherit (oldpkgs.haskell.packages.ghcjs) reflex-dom; };
   };
 
   # quasselDaemonStatic = kde5.quasselDaemon.override { static = true; };
