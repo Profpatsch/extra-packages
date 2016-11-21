ab964c04f66cb4d249f7251f987e36f4fb9d4ffa
Profpatsch <mail@profpatsch.de>
2016-11-21 10:47:23 +0100

    add ghcjs packages that track nixpkgs master

--- release.nix
+++ release.nix
@@ -23,7 +23,11 @@ in
   };
 
 
-  ghcjsPackages = with pkgs-ghcjs.haskell.packages.ghcjsHEAD; {
+  ghcjsPackages = with pkgs-ghcjs.haskell.packages.ghcjs; {
+    inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
+      smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
+  };
+  ghcjsHEADPackages = with pkgs-ghcjs.haskell.packages.ghcjsHEAD; {
     inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
       smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
   };
