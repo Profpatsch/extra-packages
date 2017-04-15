6698c35df4f6658899d6beda3ac463ba26bb9a2f
Profpatsch <mail@profpatsch.de>
2017-04-15 20:46:45 +0200

    add static haskell package tests

--- release.nix
+++ release.nix
@@ -2,7 +2,8 @@
 let
 
   pkgs = import <nixpkgs> {};
-  pkgs-ghcjs = import <nixpkgs-ghcjs> {};
+  # pkgs-ghcjs = import <nixpkgs-ghcjs> {};
+  pkgs-ghc-static = import <nixpkgs-ghc-static> {};
 
   hp = pkgs.haskellPackages.override {
     overrides = self: super: {
@@ -22,15 +23,18 @@ in
     inherit (pkgs) stack;
   };
 
+  haskell-static-compilation-test = {
+    inherit (pkgs-ghc-static.haskellPackages) purescript;
+  };
 
-  ghcjsPackages = with pkgs-ghcjs.haskell.packages.ghcjs; {
-    inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
-      smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
-  };
-  ghcjsHEADPackages = with pkgs-ghcjs.haskell.packages.ghcjsHEAD; {
-    inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
-      smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
-  };
+  # ghcjsPackages = with pkgs-ghcjs.haskell.packages.ghcjs; {
+  #   inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
+  #     smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
+  # };
+  # ghcjsHEADPackages = with pkgs-ghcjs.haskell.packages.ghcjsHEAD; {
+  #   inherit lucid Spock-api-ghcjs ghcjs-dom jsaddle-dom
+  #     smallcheck tasty-smallcheck tasty-hunit smallcheck-series;
+  # };
 
   inherit (pkgs) mkvtoolnix;
   pyqt5 = pkgs.pythonPackages.pyqt5;
